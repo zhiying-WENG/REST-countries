502b1198f51ccb6f5cb8e6b3080bf4e6d211e932
GIjoe <gijoe6688@gmail.com>
2022-01-11 18:50:41 +0100

    update 2 add page table

---
+++ js/table.js
@@ -0,0 +1,11 @@
+async function render(){
+    const reponse= await fetch("https://restcountries.com/v3.1/region/europe?fields=name,area,population,capital");
+    const countries= await reponse.json();
+    let content="";
+    countries.forEach((country) => {
+        content+=`<tr><td>${country.name.official}</td><td>${Intl.NumberFormat("en-EN").format(country.area)}</td><td>${Intl.NumberFormat("en-EN").format(country.population)}</td><td>${country.capital}</td></tr>`;     
+    });
+    const tbody=document.querySelector("#tbody");
+    tbody.innerHTML=content;
+}
+window.addEventListener("load",render);

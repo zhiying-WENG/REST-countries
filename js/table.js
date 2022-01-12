13ba6a0692aeffdc7f8272eb1335ea4de53eaed1
GIjoe <gijoe6688@gmail.com>
2022-01-12 12:29:58 +0100

    update 7 corrige table format

--- js/table.js
+++ js/table.js
@@ -1,16 +1,17 @@
 async function render(){
-    const reponse= await fetch("https://restcountries.com/v3.1/region/europe?fields=name,area,population,capital");
-    const countries= await reponse.json();
-    let content="";
+    const reponse = await fetch("https://restcountries.com/v3.1/region/europe?fields=name,area,population,capital");
+    const countries = await reponse.json();
+    let content = "";
+    const intl = new Intl.NumberFormat("en");
     countries.forEach((country) => {
         content += `<tr>
                         <td class="fw-bold">${country.name.official}</td>
-                        <td class="text-end">${country.area.toLocaleString('en-UK')}</td>
-                        <td class="text-end">${country.population.toLocaleString('en-UK')}</td>
+                        <td class="text-end">${intl.format(country.area)}</td>
+                        <td class="text-end">${intl.format(country.population)}</td>
                         <td>${country.capital[0]}</td>
                     </tr>`;     
     });
-    const tbody=document.querySelector("#tbody");
-    tbody.innerHTML=content;
+    const tbody = document.querySelector("#tbody");
+    tbody.innerHTML = content;
 }
 window.addEventListener("load",render);
